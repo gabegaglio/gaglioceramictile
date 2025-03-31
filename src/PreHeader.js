82b9071e2e4fe3744a1cc5f7caa97e51a0633b71
Gabriel Gaglio <155278142+gabegaglio@users.noreply.github.com>
2025-03-30 20:17:33 -0400

    switched pre header contact & location

--- src/PreHeader.js
+++ src/PreHeader.js
@@ -27,16 +27,16 @@ function PreHeader() {
             Gallery
           </button>
           <button
-            onClick={() => scrollToSection("contact")}
-            className="text-black bg-white/20 backdrop-blur-xl rounded-lg hover:scale-110 hover:bg-white/30 px-3 py-2 text-sm md:text-lg font-light transition-all duration-200 shadow-lg border border-white/10 w-24 sm:w-28 md:w-32 flex items-center justify-center"
-          >
-            Contact
-          </button>
-          <button
             onClick={() => scrollToSection("map")}
             className="text-black bg-white/20 backdrop-blur-xl rounded-lg hover:scale-110 hover:bg-white/30 px-3 py-2 text-sm md:text-lg font-light transition-all duration-200 shadow-lg border border-white/10 w-24 sm:w-28 md:w-32 flex items-center justify-center"
           >
             Location
+          </button>
+          <button
+            onClick={() => scrollToSection("contact")}
+            className="text-black bg-white/20 backdrop-blur-xl rounded-lg hover:scale-110 hover:bg-white/30 px-3 py-2 text-sm md:text-lg font-light transition-all duration-200 shadow-lg border border-white/10 w-24 sm:w-28 md:w-32 flex items-center justify-center"
+          >
+            Contact
           </button>
         </div>
       </nav>
